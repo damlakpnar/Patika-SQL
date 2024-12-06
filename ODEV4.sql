aea17f79bb90b0801085b3511eea1f9484f81d4c
Damla Akpınar <damlaakpinaaar@gmail.com>
2024-12-06 16:23:38 +0300

    Create ODEV4.sql

---
+++ ODEV4.sql
@@ -0,0 +1,15 @@
+-- ODEV 4
+
+-- 1-film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+SELECT DISTINCT replacement_cost FROM Film;
+
+-- 2-film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT COUNT(replacement_cost) FROM Film;
+
+-- 3-film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT COUNT(title) FROM Film
+WHERE title LIKE 'T%' AND rating= 'G';
+
+-- 4-country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT COUNT(country) FROM Country
+WHERE country LIKE '_____';
